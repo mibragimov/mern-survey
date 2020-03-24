98fe51785fac8bf0165647f9019eb406fe4f7e6c
Muhammad Ibragimov <mibragimov@hotmail.com>
2020-03-24 19:05:32 +0500

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose
+	.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('mongodb connected'))
+	.catch((err) => console.log(err));
+
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy());
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [ keys.cookieKey ]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 8080;
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
